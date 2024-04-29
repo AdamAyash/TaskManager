b76374c7cc8e03798bcfa5e66aeb065de51cf569
Adam Ayash <76952746+AdamAyash@users.noreply.github.com>
2024-04-29 08:19:02 +0300

    added Repository

--- src/main/java/bg/tuvarna/sit/taskmanager/domain_models/Task.java
+++ src/main/java/bg/tuvarna/sit/taskmanager/domain_models/Task.java
@@ -1,18 +1,29 @@
 package bg.tuvarna.sit.taskmanager.domain_models;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Setter
 
+@Entity
+@Table(name="Tasks")
+
 public class Task {
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
   private Long id;
   private String title;
   private String description;
   private String deadline;
+  @CreationTimestamp
+  private Instant createdOn;
 }
